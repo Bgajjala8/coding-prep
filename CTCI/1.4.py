6de7369653ecc6cfce31652d97b6de3c6d2104f6
Bharath Gajjala <bgajjala@nekokala.local>
2024-04-25 11:01:36 -0700

    1.4

--- CTCI/1.4.py
+++ CTCI/1.4.py
@@ -1,8 +1,40 @@
+from collections import Counter
+
+
+# Notes
+# -----
+# Counter can be brought in from collections this just returns MAP with char:count(char-in-s)
+# for string that are even numbers there should be no odds beacuse it should flip correctly 
+# from string that are odd numbers one char should repeat because %2 is not o
+# remeber when count to LOWERCASE string. Lowercasing string allows for correct calculation in this case
+# String to lower -> s.lower()  String to upper -> s.upper()
+
 # Palindrome  Permutation:  Given  a  string,  write a function  to  check  if it  is a  permutation  of a  palin-
 # drome.  A  palindrome  is a  w o r d  or  phrase  that  is  the  same  forwards  and  backwards.  A  permutation 
 # is a  rearrangement of letters. The palindrome  does  not  need  to  be  limited  to just  dictionary words. 
 
-# To be a plaindrome 
-# First would be - count each ones. Only one can be odd which would be the middle number 
-# then for permutation you would need to 
+def is_palindrome_permutation(s):
+    
+    # Detrmine weather s is a even or odd lettered 
+    is_even_length = is_even(s)
+    
+    # Count the chars 
+    char_count = Counter(s.lower()) 
+    
+    # Get odd count
+    odd_char_count = sum(1 for c in char_count.values() if not is_even(c))
+    
+    if (is_even_length and odd_char_count > 0) or odd_char_count > 1:
+        return False
+    
+    return True
+    
+    
+# Helper 
+def is_even(num):
+   return num % 2 == 0
+            
+    
+    
+    
 
